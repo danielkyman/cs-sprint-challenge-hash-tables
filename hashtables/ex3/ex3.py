f2c0419bd1d0154df6badc82555f255ba58e7bdd
danielkyman <danny.kyman@gmail.com>
2020-08-07 09:36:49 -0700

    completed mvp

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,6 +3,22 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+
+    # put all elements with their counts in dict
+    counts = {}
+
+    for values in arrays:
+        for item in values:
+            if item in counts:
+                counts[item] += 1
+            else:
+                counts[item] = 1
+
+    # get all items with count same as number of elements in arrays
+    for item in counts:
+        if counts[item] == len(arrays):
+            result.append(item)
 
     return result
 
